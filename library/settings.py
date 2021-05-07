02d9855739d6f1882b45756e1fefbb9dc10df2fa
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-07 12:31:40 +0100

    change login redirect to root

--- library/settings.py
+++ library/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+LOGIN_REDIRECT_URL = 'bookshelf-index'
+
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
