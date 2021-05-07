158362b243fa7d123568ea8f1ad3690cb0dc420d
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-07 11:43:57 +0100

    add paths for signup, login and logout

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('bookshelf.urls')),
     path('admin/', admin.site.urls),
+    path('signup/', user_views.register, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
