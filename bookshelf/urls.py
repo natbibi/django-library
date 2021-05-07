83bf937490f4e64d2c2c306baf75a8cdc01baeb8
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-07 12:55:16 +0100

    add path /authors to render a list of all authors

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='bookshelf-index'), # route for /adoption/
     path('about/', views.about, name='bookshelf-about'), # route for /adoption/about
     path('books/', views.books, name='bookshelf-books'), # route for /adoption/:id
+    path('authors/', views.authors, name='bookshelf-authors'), # route for /adoption/:id
     # path('<id>/', views.show, name='bookshelf-show'), # route for /adoption/:id
     path('<int:id>/', views.show, name='bookshelf-show') # to accept only numbers as id param
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from .models import Book
+from .models import Book, Author
 
 def index(req):
     return render(req, 'base.html') 
@@ -18,3 +18,8 @@ def show(req, id):
 def books(req):
     books = {'books': Book.objects.all()}
     return render(req, 'show.html', books) 
+
+@login_required
+def authors(req):
+    authors = {'authors': Author.objects.all()}
+    return render(req, 'authors.html', authors) 
