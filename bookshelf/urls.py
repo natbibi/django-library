5785e596e6c8e0b06dcfe645d7f3542d6ebb589e
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-07 12:43:32 +0100

    add new path for books

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='bookshelf-index'), # route for /adoption/
-    # path('about/', views.about, name='bookshelf-about'), # route for /adoption/about
-    # path('<id>/', views.show, name='adoption-show'), # route for /adoption/:id
+    path('about/', views.about, name='bookshelf-about'), # route for /adoption/about
+    path('books/', views.books, name='bookshelf-books'), # route for /adoption/:id
+    # path('<id>/', views.show, name='bookshelf-show'), # route for /adoption/:id
     path('<int:id>/', views.show, name='bookshelf-show') # to accept only numbers as id param
 ]
