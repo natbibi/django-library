1c6b4c1fb48ed91900ea17c21423d9ba250146d6
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-07 12:45:24 +0100

    add auth and new routes

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,4 +1,5 @@
 # adoption/views.py
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import Book
@@ -6,6 +7,14 @@ from .models import Book
 def index(req):
     return render(req, 'base.html') 
 
+def about(req):
+    return render(req, 'about.html') 
+
 def show(req, id):
     book = Book.objects.get(pk=id)
     return HttpResponse(f'<h3>Book number {id}!</h3><p>{book.name} by {book.author}</p>')
+
+@login_required
+def books(req):
+    books = {'books': Book.objects.all()}
+    return render(req, 'show.html', books) 
