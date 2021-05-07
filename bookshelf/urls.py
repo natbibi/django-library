61f5a555a1817ecd032e5d79e2ca6d4f220f3bfc
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-07 15:22:30 +0100

    create routes for author and books, and then add new book button linked to authors

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('books/', views.books, name='bookshelf-books'), # route for /adoption/:id
     path('authors/', views.authors, name='bookshelf-authors'), # route for /adoption/:id
     # path('<id>/', views.show, name='bookshelf-show'), # route for /adoption/:id
-    path('<int:id>/', views.show, name='bookshelf-show') # to accept only numbers as id param
+    path('books/<int:id>/', views.showbook, name='bookshelf-show'), # to accept only numbers as id param
+    path('books/new/', views.create, name='book-create'),
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -4,22 +4,37 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import Book, Author
 
+from .forms import NewBookForm
+
 def index(req):
     return render(req, 'base.html') 
 
 def about(req):
     return render(req, 'about.html') 
 
-def show(req, id):
-    book = Book.objects.get(pk=id)
-    return HttpResponse(f'<h3>Book number {id}!</h3><p>{book.name} by {book.author}</p>')
+def showbook(req, id):
+    book = {'book': Book.objects.get(pk=id)}
+    return render(req, 'showbook.html', book) 
+    # return HttpResponse(f'<h3>Book number {id}!</h3><p>{book.name} by {book.author}</p>')
 
 @login_required
 def books(req):
     books = {'books': Book.objects.all()}
-    return render(req, 'show.html', books) 
+    return render(req, 'books.html', books) 
 
 @login_required
 def authors(req):
     authors = {'authors': Author.objects.all()}
     return render(req, 'authors.html', authors) 
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        book = NewBookForm(request.POST)
+        if book.is_valid():
+            book_id = book.save().id
+            return redirect("bookshelf-books")
+    else:
+        form = NewBookForm()
+    data = {'form': form}
+    return render(request, 'newbook.html', data)
